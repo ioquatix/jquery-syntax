8d1d99b997514ea21e3449c350e3af6fd4ad77da
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2010-02-14 17:20:36 +1300

    Improved table layout.

--- jquery.syntax.layout.table.js
+++ jquery.syntax.layout.table.js
@@ -1,22 +1,98 @@
+function createWindow (url, name, width, height, options) {
+	var x = (screen.width - width) / 2, y = (screen.height - height) / 2;
+		
+	//options +=	',left=' + x +
+	//				',top=' + y +
+	//				',width=' + width +
+	//				',height=' + height;
+	//				
+	options = options.replace(/^,/, '');
+
+	var win = window.open(url, name, options);
+	
+	win.focus();
+	
+	return win;
+}
+
 Syntax.layouts.table = function(options, code, container) {
-	var table = $('<table>'), tr = null, td = null;
+	var table = $('<table>'), tr = null, td = null, a = null, toolbar = null
 	var line = 1;
 	
 	table.addClass('syntax');
 	
+	// Toolbar
+	toolbar = document.createElement('div');
+	toolbar.className = "toolbar";
+	
+	a = document.createElement('a');
+	a.href = "#";
+	a.innerHTML = "View Raw Code";
+	
+	a.onclick = function() {
+		var win = createWindow('#', '_blank', 700, 500, 'location=0, resizable=1, menubar=0, scrollbars=1, title=Raw');
+
+		win.document.write('<html><head></head><body><pre class="syntax">' + code.html() + '</pre></body></html>');
+
+		$('link').each(function(){
+			if (this.rel != 'stylesheet') {
+				return;
+			}
+			
+			window.console.log(this.href);
+			var link = $('<link rel="stylesheet">', win.document);
+			
+			link.attr('type', this.type);
+			link.attr('href', this.href);
+			link.attr('media', this.media);
+			
+			$("head", win.document).append(link);
+		});
+		
+		debugger
+		
+		win.document.close();
+		win.document.title = "Raw Source Code";
+	};
+	
+	toolbar.appendChild(a);
+
+	a = document.createElement('a');
+	a.href = "http://www.oriontransfer.co.nz/software/jquery-syntax/";
+	a.innerHTML = "?";
+	toolbar.appendChild(a);
+	
+	// Source code
 	code.children().each(function() {
-		tr = $('<tr>').addClass('line', 'line-' + line);
+		tr = document.createElement('tr');
+		tr.className = "line ln" + line;
 		
-		td = $('<td>').addClass('number').text(line);
-		tr.append(td);
+		if (line % 2) {
+			tr.className += " alt";
+		}
 		
-		td = $('<td>').addClass('source');
-		td.append(this);
-		tr.append(td);
+		td = document.createElement('td');
+		td.className = "number";
+		td.innerHTML = line;
+		tr.appendChild(td);
 		
-		table.append(tr);
+		td = document.createElement('td');
+		td.className = "source " + this.className;
+		td.innerHTML = this.innerHTML;
+		tr.appendChild(td);
+		
+		if (toolbar) {
+			td.appendChild(toolbar);
+			toolbar = null;
+		}
+		
+		table[0].appendChild(tr);
 		line = line + 1;
 	});
 	
+	$('.href', table).each(function(){
+		$(this).replaceWith($('<a>').attr('href', this.innerHTML).text(this.innerHTML));
+	});
+	
 	return table;
-}
+};
