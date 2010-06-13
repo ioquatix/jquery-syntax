2c49a106a83bcf2443853f0ca980fb02eb5b197e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2010-06-13 23:55:00 +1200

    Added better handling of strings, numbers and named constants.

--- jquery.syntax.brush.asm.js
+++ jquery.syntax.brush.asm.js
@@ -18,10 +18,21 @@ Syntax.register('asm', function(brush) {
 	brush.push({pattern: /^[a-zA-Z_][a-zA-Z0-9_]*:/gm, klass: 'label'});
 	
 	brush.push({pattern: /(-[0-9]+)|(\b[0-9]+)|(\$[0-9]+)/g, klass: 'constant'});
-	brush.push({pattern: /(\-|\b|\$)(0x[0-9a-f]+|[0-9]+)/g, klass: 'constant'});
+	brush.push({pattern: /(\-|\b|\$)(0x[0-9a-f]+|[0-9]+|[a-z0-9_]+)/gi, klass: 'constant'});
+	brush.push({pattern: /$[a-zA-Z_][a-zA-Z0-9_]*:/gm, klass: 'label'});
 	
 	brush.push({pattern: /%\w+/g, klass: 'register'});
 	
+	// Strings
+	brush.push(Syntax.lib.singleQuotedString);
+	brush.push(Syntax.lib.doubleQuotedString);
+	brush.push(Syntax.lib.stringEscape);
+	
+	// Numbers
+	brush.push(Syntax.lib.decimalNumber);
+	brush.push(Syntax.lib.hexNumber);
+	
+	// Comments
 	brush.push(Syntax.lib.perlStyleComment);
 	brush.push(Syntax.lib.webLink);
 });
