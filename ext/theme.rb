d16d7f728c282a84150860cbe14b056b74a020c9
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-02-02 04:41:13 +1300

    Better structure for rakefile processes.

---
+++ ext/theme.rb
@@ -0,0 +1,65 @@
+
+class Theme
+	def initialize(dst_dir, root = nil)
+		@destination = dst_dir
+		
+		@includes = []
+		@root = root
+		
+		@extends = {}
+	end
+	
+	attr :includes
+	
+	def load_theme(theme_dir)
+		# Themes which are relative paths are designed to reside in the themes subdirectory.
+		unless File.directory?(theme_dir)
+			theme_dir = File.join(@root, theme_dir)
+		end
+	
+		unless File.directory?(theme_dir)
+			raise StandardError.new("Could not find theme #{theme_dir}!")
+		end
+	
+		$stderr.puts "Loading theme from #{theme_dir}..."
+		theme_config_path = File.join(theme_dir, "config.yaml")
+		config = {}
+	
+		# Is there a configuration file?
+		if File.exist? theme_config_path
+			config = YAML::load_file(theme_config_path)
+		end
+	
+		# Load any dependencies recursively - if you have bad configuration this might
+		# give you visions of infinity.
+		if config['depends']
+			config['depends'].each {|name| load_theme(name)}
+		end
+		
+		if config['extends']
+			config['extends'].each {|name,extension| @extends[name] = extension}
+		end
+		
+		if config['includes']
+			@includes.concat(config['includes'])
+		end
+	
+		# Copy all the theme files
+		FileUtils.cp_r(Dir.glob(theme_dir + "/*"), @destination)
+	
+		# Remove any files/directories that have been excluded
+		if config['exclude']
+			config['exclude'].each do |name|
+				FileUtils.rm_rf(File.join(@destination, name))
+			end
+		end
+	end
+	
+	def includes_for(path)
+		if extension = @extends[File.basename(path)]
+			return @includes + [extension]
+		else
+			return @includes
+		end
+	end
+end
