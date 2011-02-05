ead50b7f88a388c495132f1448b6830584da26ca
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-02-06 03:08:57 +1300

    Update public deployment.

--- public/jquery.syntax.brush.yaml.js
+++ public/jquery.syntax.brush.yaml.js
@@ -2,4 +2,4 @@
 // Copyright 2010 Samuel Williams. All rights reserved.
 // For more information, please see <http://www.oriontransfer.co.nz/software/jquery-syntax>
 
-Syntax.register('yaml',function(brush){brush.push({pattern:/^\s*(#.*)$/gm,matches:Syntax.extractMatches({klass:'comment',allow:['href']})});brush.push(Syntax.lib.singleQuotedString);brush.push(Syntax.lib.doubleQuotedString);brush.push({pattern:/(&|\*)[a-z0-9]+/gi,klass:'constant'});brush.push({pattern:/(.*?):/gi,matches:Syntax.extractMatches({klass:'keyword'})});brush.push(Syntax.lib.webLink);});
+Syntax.register('yaml',function(brush){brush.push({pattern:/^\s*#.*$/gm,klass:'comment',allow:['href']});brush.push(Syntax.lib.singleQuotedString);brush.push(Syntax.lib.doubleQuotedString);brush.push({pattern:/(&|\*)[a-z0-9]+/gi,klass:'constant'});brush.push({pattern:/(.*?):/gi,matches:Syntax.extractMatches({klass:'keyword'})});brush.push(Syntax.lib.webLink);});
