a9c568034aeef254d7e8bcc5580db327660024cd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-05-10 00:07:46 +1200

    Added preliminary support for Go, OCaml/F#, Protocol Buffers and Super Collider.

---
+++ source/jquery.syntax.brush.super-collider.js
@@ -0,0 +1,50 @@
+// brush: "super-collider" aliases: ["sc"]
+
+//	This file is part of the "jQuery.Syntax" project, and is distributed under the MIT License.
+//	Copyright (c) 2011 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
+//	See <jquery.syntax.js> for licensing details.
+
+Syntax.register('super-collider', function(brush) {
+	var keywords = ["arg", "classvar", "var"];
+	var operators = ["`", "+", "@", ":", "*", "/", "-", "&", "|", "~", "!", "%", "<", "=", ">"];
+	var values = ["self", "super", "true", "false", "nil", "inf"];
+		
+	brush.push(keywords, {klass: 'keyword'});
+	brush.push(operators, {klass: 'operator'});
+	brush.push(values, {klass: 'constant'});
+	
+	brush.push(Syntax.lib.camelCaseType);
+	
+	brush.push({
+		pattern: /\$./g,
+		klass: "constant"
+	});
+	
+	brush.push({
+		pattern: /\\[a-z]+/gi,
+		klass: "constant"
+	});
+	
+	// Comments
+	brush.push(Syntax.lib.cStyleComment);
+	brush.push(Syntax.lib.cppStyleComment);
+	brush.push(Syntax.lib.webLink);
+	
+	// Strings
+	brush.push(Syntax.lib.singleQuotedString);
+	brush.push(Syntax.lib.doubleQuotedString);
+	brush.push(Syntax.lib.stringEscape);
+	
+	// Numbers
+	brush.push(Syntax.lib.decimalNumber);
+	brush.push(Syntax.lib.hexNumber);
+	
+	// Functions
+	brush.push({
+		pattern: /(?:\.)([a-z_][a-z0-9_]+)/gi, 
+		matches: Syntax.extractMatches({klass: 'function'})
+	});
+	
+	brush.push(Syntax.lib.cStyleFunction);
+});
+
