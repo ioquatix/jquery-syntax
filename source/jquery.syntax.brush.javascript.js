4aa479a60c7f5159a76a92aa8d9a7b6513ba2ac9
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-04-12 21:35:26 +0900

    ES9 keywords for JavaScript.

--- source/jquery.syntax.brush.javascript.js
+++ source/jquery.syntax.brush.javascript.js
@@ -1,18 +1,21 @@
 // brush: "javascript" aliases: ["js", "actionscript"]
 
 //	This file is part of the "jQuery.Syntax" project, and is distributed under the MIT License.
-//	Copyright (c) 2011 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
+//	Copyright (c) 2019 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
 //	See <jquery.syntax.js> for licensing details.
 
 Syntax.register('javascript', function(brush) {
-	var keywords = ["function", "break", "case", "catch", "continue", "default", "delete", "do", "else", "for", "if", "in", "instanceof", "new", "return", "super", "switch", "throw", "true", "try", "typeof", "var", "while", "with", "prototype"];
+	var keywords = ["async", "await", "break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "export", "extends", "finally", "for", "function", "if", "import", "in", "instanceof", "let", "new", "return", "super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield"];
 	
 	var operators = ["+", "*", "/", "-", "&", "|", "~", "!", "%", "<", "=", ">"];
 	var values = ["this", "true", "false", "null"];
 	
+	var access = ["implements", "package", "protected", "interface", "private", "public"];
+	
 	brush.push(values, {klass: 'constant'});
 	brush.push(keywords, {klass: 'keyword'});
 	brush.push(operators, {klass: 'operator'});
+	brush.push(access, {klass: 'access'});
 	
 	// Regular expressions
 	brush.push(Syntax.lib.perlStyleRegularExpression);
@@ -37,4 +40,3 @@ Syntax.register('javascript', function(brush) {
 	// Functions
 	brush.push(Syntax.lib.cStyleFunction);
 });
-
