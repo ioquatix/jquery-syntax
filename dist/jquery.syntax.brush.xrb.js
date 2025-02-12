0b0b4441207b49d6a399c814492b3287f2f9d5ca
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-12 14:50:15 +1300

    Update `dist`.

---
+++ dist/jquery.syntax.brush.xrb.js
@@ -0,0 +1,2 @@
+// This file is part of the "jQuery.Syntax" project, and is distributed under the MIT License.
+Syntax.brushes.dependency("xrb","xml");Syntax.brushes.dependency("xrb","ruby");Syntax.register("xrb",function(a){a.push({pattern:/((<\?r)([\s\S]*?)(\?>))/gm,matches:Syntax.extractMatches({klass:"ruby-tag",allow:["keyword","ruby"]},{klass:"keyword"},{brush:"ruby"},{klass:"keyword"})});a.push({pattern:/((#{)([\s\S]*?)(}))/gm,matches:Syntax.extractMatches({klass:"ruby-tag",allow:["keyword","ruby"]},{klass:"keyword"},{brush:"ruby"},{klass:"keyword"})});a.derives("xml")});
